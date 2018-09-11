4032deb0fabc579bf2f24e80d3e49638befc356e
dipeshsinha <dipeshsinha@users.noreply.github.com>
2018-09-11 23:43:47 +0530

    changed signup

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {NgForm} from '@angular/forms';
 import {ServerService} from '../server.service';
+import {Router, RouterEvent} from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -12,7 +13,7 @@ export class SignupComponent implements OnInit {
   @ViewChild('pas') pas;
   @ViewChild('repas') repas;
 
-  constructor(private req: ServerService) { }
+  constructor(private req: ServerService, private router: Router) { }
 
   ngOnInit() {
   }
@@ -21,7 +22,8 @@ export class SignupComponent implements OnInit {
       'password': form.value.password}) // function in serverservice to signup user and login
       .subscribe(
         (response: {token: string}) => {this.req.token = response.token; // getting token of signrd up user
-        this.req.loggedin.emit('users'); // changing navbar to main navbar
+        this.req.loggedin.emit('yes');
+          this.router.navigate(['/users']); // changing navbar to main navbar
         },
         (error) => alert('An error occured, please try again'));
   }
